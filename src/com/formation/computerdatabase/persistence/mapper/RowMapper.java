539a254389345d53a54efb9fd1e4684b741efa1e
pp <ppbeauchemin@excilys.com>
2014-12-30 11:02:56 +0100

    add rowMapper and dao

--- src/com/formation/computerdatabase/persistence/mapper/RowMapper.java
+++ src/com/formation/computerdatabase/persistence/mapper/RowMapper.java
@@ -6,12 +6,11 @@ import java.util.List;
 
 public interface RowMapper<T> {
 
+	static final String COMPUTER_PREFIX = "cp.";
+	static final String COMPANY_PREFIX = "ca.";
+
 	T mapRow(ResultSet rs) throws SQLException;
 
-	T mapRow(ResultSet rs, String prefix) throws SQLException;
-
 	List<T> mapRows(ResultSet rs) throws SQLException;
-
-	List<T> mapRows(ResultSet rs, String prefix) throws SQLException;
 
 }
