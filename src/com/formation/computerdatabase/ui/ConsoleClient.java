4c17d787bc84f1aa3316212844525bbe881ed195
pif <pffiat@excilys.com>
2014-12-31 12:18:32 +0100

    fix rowmapper & fix daoS & fix clientConsole

--- src/com/formation/computerdatabase/ui/ConsoleClient.java
+++ src/com/formation/computerdatabase/ui/ConsoleClient.java
@@ -11,6 +11,10 @@ import com.formation.computerdatabase.service.ServiceFactory;
 import com.formation.computerdatabase.util.DateUtil;
 
 public class ConsoleClient {
+	
+	private boolean continueApplication = true;
+
+	private final Scanner scanner = new Scanner(System.in);
 
 	private ComputerDatabaseService service;
 
@@ -28,11 +32,10 @@ public class ConsoleClient {
 		System.out.println("d) Add computer");
 		System.out.println("e) Edit computer");
 		System.out.println("f) Delete computer");
+		System.out.println("x) Exit application");
 
 		// Option regex
 		Pattern p = Pattern.compile("^[a-f]$");
-
-		Scanner scanner = new Scanner(System.in);
 		String s = null;
 
 		do {
@@ -59,8 +62,10 @@ public class ConsoleClient {
 		case "f":
 			deleteComputer();
 			break;
+		case "x":
+			exitApplication();
+			break;
 		}
-		scanner.close();
 	}
 
 	private void showAllComputers() {
@@ -71,6 +76,7 @@ public class ConsoleClient {
 		for (Computer c : computers) {
 			System.out.println(new StringBuilder().append(c.getId()).append(" - ").append(c.getName()).toString());
 		}
+		this.anyKeyToMenu();
 	}
 
 	private void showAllCompanies() {
@@ -81,19 +87,19 @@ public class ConsoleClient {
 		for (Company c : companies) {
 			System.out.println(new StringBuilder().append(c.getId()).append(" - ").append(c.getName()).toString());
 		}
+		this.anyKeyToMenu();
 	}
 
 	private void showComputer() {
 		System.out.println("Computer details:");
-		Scanner scanner = new Scanner(System.in);
 		Long l = null;
 
 		System.out.println("Enter computer id: ");
 		l = scanner.nextLong();
-		scanner.close();
 		Computer c = service.retrieveOneComputer(l);
 
 		System.out.println(c.toString());
+		this.anyKeyToMenu();
 
 	}
 
@@ -125,11 +131,11 @@ public class ConsoleClient {
 		Computer c = b.build();
 		service.saveComputer(c);
 		System.out.println("Computer created with id:" + c.getId());
+		this.anyKeyToMenu();
 	}
 
 	public void editComputer() {
 		System.out.println("Computer edition:");
-		Scanner scanner = new Scanner(System.in);
 		Long l = null;
 		String input = null;
 
@@ -167,48 +173,61 @@ public class ConsoleClient {
 		
 		service.saveComputer(c);
 		System.out.println("Computer edited.");
+		this.anyKeyToMenu();
 
 	}
 
 	public void deleteComputer() {
 		System.out.println("Computer to delete:");
-		Scanner scanner = new Scanner(System.in);
 		Long l = null;
 
 		System.out.println("Enter computer id: "); 
 		l = scanner.nextLong();
-		scanner.close();
 		
 		service.deleteComputer(l);
 
 		System.out.println(l+" is deleted.");
+		this.anyKeyToMenu();
 	}
 	
 	private String getInput(String command) {
-		final Scanner scanner = new Scanner(System.in);
 		String input = null;
 
 		do {
 			System.out.println(command);
-			input = scanner.next().trim();
+			input = scanner.next();
 		} while (input.isEmpty());
-		scanner.close();
 		return input;
 	}
 	
 	private String getInputWithPattern(String command, Pattern pattern) {
-		final Scanner scanner = new Scanner(System.in);
+		System.out.println(command);
 		String input = null;
 		do {
-			System.out.println("Enter introduced date (yyyy-mm-dd) or leave empty: ");
 			input = scanner.next().trim();
-		} while (!input.isEmpty() && !pattern.matcher(input).find());
-		scanner.close();
+		} while ( !pattern.matcher(input).find());
 		return input;
+	}
+
+	private void exitApplication() {
+		scanner.close();
+		this.continueApplication = false;
+		System.out.println("The application was correctly closed.");
+	}
+
+	private void anyKeyToMenu() {
+		System.out.println("Press any key to return to the main menu...");
+		String input = null;
+		do {
+			input = scanner.next();
+		} while (input.isEmpty());
 	}
 
 	public static void main(String[] args) {
 		ConsoleClient c = new ConsoleClient();
-		
+		while(c.continueApplication) {
+			c.showMenu();
+			System.out.println("one main loop");
+		}
 	}
 }
