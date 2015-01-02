af00cb824daeaba8caf85af23648db200a52a2a2
benoitboyer <bboyer@excilys.com>
2015-01-02 14:24:56 +0100

    Added simple Pageable

---
+++ src/com/formation/computerdatabase/commons/Pageable.java
@@ -0,0 +1,34 @@
+package com.formation.computerdatabase.commons;
+
+import java.util.List;
+
+public class Pageable<T> {
+	
+	private List<T> computers;
+	private int page = 1;
+	private int size = 10;
+	
+	
+	/*--------------------------------------------------------------
+     * Getter/Setter
+     --------------------------------------------------------------*/
+	public List<T> getComputers() {
+		return computers;
+	}
+	public void setComputers(List<T> computers) {
+		this.computers = computers;
+	}
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+}
--- src/com/formation/computerdatabase/persistence/ComputerDao.java
+++ src/com/formation/computerdatabase/persistence/ComputerDao.java
@@ -1,7 +1,14 @@
 package com.formation.computerdatabase.persistence;
 
+import java.util.List;
+
+import com.formation.computerdatabase.exception.DaoStubException;
 import com.formation.computerdatabase.model.Computer;
 
 public interface ComputerDao extends Dao<Computer> {
 	static final String KEY = "computerDao";
+
+	default List<Computer> retrieveComputersWithOffsetAndLimit(int page, int size) {
+		throw new DaoStubException(); 
+	}
 }
